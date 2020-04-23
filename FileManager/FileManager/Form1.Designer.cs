758ae6a330307dfd436cac4cd4e4487595b32d04
Macqusha <md9793@ya.ru>
2020-04-23 20:40:18 +0700

    Load_from_Catalog_Click + cosmetics
    dataGridView -> MyDataGridView (renamed)

--- FileManager/FileManager/Form1.Designer.cs
+++ FileManager/FileManager/Form1.Designer.cs
@@ -32,12 +32,12 @@
             this.but_Load_from_DB = new System.Windows.Forms.Button();
             this.but_Load_from_Catalog = new System.Windows.Forms.Button();
             this.but_Synchronize = new System.Windows.Forms.Button();
-            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.MyDataGridView = new System.Windows.Forms.DataGridView();
             this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FileChanged = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FileCreated = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // but_Load_from_DB
@@ -62,18 +62,18 @@
             this.but_Synchronize.UseVisualStyleBackColor = true;
             this.but_Synchronize.Click += new System.EventHandler(this.but_Synchronize_Click);
             // 
-            // dataGridView
+            // MyDataGridView
             // 
-            this.dataGridView.BackgroundColor = System.Drawing.Color.MintCream;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MyDataGridView.BackgroundColor = System.Drawing.Color.MintCream;
+            this.MyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.FileName,
             this.FileSize,
             this.FileChanged,
             this.FileCreated});
-            resources.ApplyResources(this.dataGridView, "dataGridView");
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowTemplate.Height = 24;
+            resources.ApplyResources(this.MyDataGridView, "MyDataGridView");
+            this.MyDataGridView.Name = "MyDataGridView";
+            this.MyDataGridView.RowTemplate.Height = 24;
             // 
             // FileName
             // 
@@ -104,12 +104,12 @@
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::FileManager.Properties.Resources.main_background;
-            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.MyDataGridView);
             this.Controls.Add(this.but_Synchronize);
             this.Controls.Add(this.but_Load_from_Catalog);
             this.Controls.Add(this.but_Load_from_DB);
             this.Name = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -118,7 +118,7 @@
         private System.Windows.Forms.Button but_Load_from_DB;
         private System.Windows.Forms.Button but_Load_from_Catalog;
         private System.Windows.Forms.Button but_Synchronize;
-        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridView MyDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
         private System.Windows.Forms.DataGridViewTextBoxColumn FileSize;
         private System.Windows.Forms.DataGridViewTextBoxColumn FileChanged;
--- FileManager/FileManager/Form1.cs
+++ FileManager/FileManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,25 @@ namespace FileManager
 
         private void but_Load_from_Catalog_Click(object sender, EventArgs e)
         {
+            DirectoryInfo directory = new DirectoryInfo(@"E:\Rep_SecureDB_FILES-APP\Poems");
 
+            if (!directory.Exists)
+            {
+                MessageBox.Show("Каталог не найден.");
+                return;
+            }
+
+            MyDataGridView.RowCount = 1;
+            int index = 0;
+            foreach (var file in directory.GetFiles()) 
+            {
+                MyDataGridView.RowCount++;
+                MyDataGridView.Rows[index].Cells[0].Value = file.Name;
+                MyDataGridView.Rows[index].Cells[1].Value = file.Length;
+                MyDataGridView.Rows[index].Cells[2].Value = file.LastWriteTime; 
+                MyDataGridView.Rows[index].Cells[3].Value = file.CreationTime;
+                index++;                
+            }
         }
 
         private void but_Synchronize_Click(object sender, EventArgs e)
